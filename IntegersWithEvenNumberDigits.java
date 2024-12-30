ee38a1161b00d2b76c870e542a307ccfc53876dc
ngbcoder94 <ngbcoder94@gmail.com>
2024-12-30 12:09:53 -0500

    exploreDigits function created

--- IntegersWithEvenNumberDigits.java
+++ IntegersWithEvenNumberDigits.java
@@ -7,6 +7,15 @@
  */
 public class IntegersWithEvenNumberDigits{
 
+        /*
+         * Here is the main function for this solution - ExploreDigits
+         * @params nums[] - Integer array that will contain the numbers in question
+         * @return int - How many of the numbers are composed of an EVEN number of digits. 
+         */
+        public int expoloreDigits(int[] nums){
+
+        }
+
         //Main function used for testing.
         public static void main(String[] args){
                 
