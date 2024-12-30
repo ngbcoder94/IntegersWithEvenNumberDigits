2b707d19dcd0c7a474ffdd4178f9577f38edff89
ngbcoder94 <ngbcoder94@gmail.com>
2024-12-30 12:02:19 -0500

    IntegersWithEvenNumberDigits.java created

---
+++ IntegersWithEvenNumberDigits.java
@@ -0,0 +1,15 @@
+/*
+ * This is the IntegersWithEvenNumberDigits class that will be used to solve this problem. 
+ *
+ * Author: Nick Burkett
+ * Date: 12/30/24
+ *
+ */
+public class IntegersWithEvenNumberDigits{
+
+        //Main function used for testing.
+        public static void main(String[] args){
+                
+                System.out.println("Lets Test!!!!");
+        }
+}
